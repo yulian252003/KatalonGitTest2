243cfd04707ee88b65050fea721a3b8725806f92
julian <yulian252003@hotmail.com>
2021-07-12 19:20:56 -0500

    Test JuliKatalon

---
+++ Test Suites/Testcase1Tom.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testcase1Tom</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>11f18037-9c81-4ca4-a632-959b9240a466</testSuiteGuid>
+   <testCaseLink>
+      <guid>7da162eb-d093-4287-bab7-ac6493aa2a0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
